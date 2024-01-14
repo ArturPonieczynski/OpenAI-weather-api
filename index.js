1577e88f33d976c9ccf0902a63f8c0947f6c6dd0
JakubKrol <contact@itdotfocus.com>
2024-01-14 14:39:29 +0100

    First homework

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const OpenAI = require('openai');
 
-const countPriceUsd = (usage) => {
-    return usage.prompt_tokens / 1000 * 0.001 + usage.completion_tokens / 1000 * 0.002;
-}
+const openai = new OpenAI({
+    apiKey: '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',
+});
 
-(async () => {
-
-    const openai = new OpenAI({
-        apiKey: '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',
-    });
-
+const getSentiment = async (text, model = 'gpt-3.5-turbo') => {
     console.log('Pytam AI...');
 
     const completion = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: [
             {
-                role: 'user', content: 'Powiedz jakiś krótki żart. Ma być zabawny!',
+                role: 'user', content: `Napisz jaki jest sentyment poniższego tekstu. Odpowiedz, że jest pozytywny, neutralny lub negatywny. Oto tekst:\n\n---\n\n${text}`,
             },
         ],
     });
 
-    console.log(completion?.choices?.[0]?.message?.content ?? 'Brak odpowiedzi!');
-    console.log('$', countPriceUsd(completion.usage));
+    return completion?.choices?.[0]?.message?.content ?? 'Brak odpowiedzi!';
+}
+
+(async () => {
+
+    console.log(await getSentiment('Bardzo podoba mi się kurs mistrz.ai!'));
+    console.log(await getSentiment('Kurs mistrz.ai trwa do ok. kwietnia 2024.'));
 
     /**
      * Zadanie domowe:
